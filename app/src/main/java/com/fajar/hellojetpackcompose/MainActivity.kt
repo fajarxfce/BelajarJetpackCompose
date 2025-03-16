849a7ad4fa4e88b548dacae0f6101261ebd2f5e4
fajarxfce <nurul.10120014@mahasiswa.unikom.ac.id>
2025-03-16 10:33:57 +0700

    chore(android): update project files and dependencies

--- app/src/main/java/com/fajar/hellojetpackcompose/MainActivity.kt
+++ app/src/main/java/com/fajar/hellojetpackcompose/MainActivity.kt
@@ -9,20 +9,27 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -36,6 +43,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -65,8 +74,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, device = "id:pixel_5")
-@Preview(showBackground = true, device = "id:pixel_5", uiMode = UI_MODE_NIGHT_YES)
+//@Preview(showBackground = true, device = "id:pixel_5")
+//@Preview(showBackground = true, device = "id:pixel_5", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun HelloJetpackComposeApp() {
     GreetingList(sampleName)
@@ -133,3 +142,64 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+fun Weight() {
+    Column(modifier = Modifier
+        .padding(16.dp)) {
+        Row {
+            Button({}, Modifier.weight(1f)) { }
+            Button({}) { }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+private fun PreviewWeight() {
+    Weight()
+}
+
+@Composable
+fun ContactCard(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(onClick = {})
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = "Avatar",
+            modifier = Modifier
+                .padding(4.dp)
+                .border(2.dp, Color.Green, CircleShape)
+                .clip(CircleShape)
+                .size(60.dp)
+
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "Fajar Tampan",
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Online",
+            )
+        }
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = null,
+            modifier = Modifier.offset(x = 6.dp, y = 25.dp)
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+private fun ContactCardPreview() {
+    ContactCard("Fajar")
+}
+
